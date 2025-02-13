6766dd435383006704a436a03cd9dbeccf959f3b
gthrepwood <gthrepwood@gmail.com>
2025-02-13 22:38:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,4 +8,25 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-alert("hello")
+function startCountdown(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = 0;
+    }
+  }, 1000);
+}
+
+window.onload = function () {
+  var twoMinutes = 60 * 2,
+      display = document.querySelector('#time');
+  startCountdown(twoMinutes, display);
+};
